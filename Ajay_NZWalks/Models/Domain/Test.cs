74e1a7a76f21c417181f30a882c9d7ca916b429b
AjayChoughule <ajaychoughulee@gmail.com>
2025-05-20 15:15:23 +0530

    removed class

--- Ajay_NZWalks/Models/Domain/Test.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ajay_NZWalks.Models.Domain
-{
-    public class Test
-    {
-        public int MyProperty { get; set; }
-        public int MyProperty1 { get; set; }
-        public int MyProperty2 { get; set; }
-    }
-}
